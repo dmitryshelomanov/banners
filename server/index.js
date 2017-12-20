1ea08d921e807853934bc8b7eb836cc557a3c61d
Shelomanov <i.ivanov@idea-promotion.ru>
2017-12-20 11:09:17 +0300

    add base64 convert

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const debug = require('debug')('banner:index')
 const Koa = require('koa')
 const Router = require('koa-router')
 const middleware = require('./src/middleware')
-const fs = require('fs-extra')
-const { tempPath } = require('./src/helpers')
 const {
   uploadBanner,
   compressImg,
   compressArchive,
   parseBanner,
+  base64Convert,
 } = require('./src/controllers')
 
 
@@ -19,25 +18,7 @@ compressImg(router, '/compress/img')
 uploadBanner(router, '/upload')
 compressArchive(router, '/compress/archive')
 parseBanner(router, '/parse/banner')
-
-router.post('/base64', async (ctx) => {
-  const { body } = ctx.request
-  const { process } = tempPath()
-  const data = JSON.parse(body.data)
-
-  try {
-    data.forEach(async (i, k) => {
-      const b = i.replace(/^data:image\/jpeg;base64,/, '')
-
-      await fs.writeFile(process(`8b9a9c0bcfd045ef19e38861968cef3a--240x400.zip\\test\\${k}.jpeg`), b, 'base64')
-    })
-  }
-  catch (error) {
-    ctx.throw(error)
-  }
-
-  ctx.body = body.data.lenght
-})
+base64Convert(router, '/base64')
 
 middleware(app)
 
--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,6 +2,7 @@ const uploadBanner = require('./upload-banner')
 const compressImg = require('./compress-img')
 const compressArchive = require('./compress-archive')
 const parseBanner = require('./parse-banner')
+const base64Convert = require('./base64-convert')
 
 
 module.exports = {
@@ -9,4 +10,5 @@ module.exports = {
   compressImg,
   compressArchive,
   parseBanner,
+  base64Convert,
 }
