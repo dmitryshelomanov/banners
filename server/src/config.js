e32707b0963b8234d1ee2c0abdc5a54fce0a5d62
Shelomanov <yzdrf98@mail.ru>
2018-01-16 14:35:45 +0300

    update config

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,12 @@
+const dbConfig = require('./database.json')
+
+const {
+  NODE_ENV = 'development',
+} = process.env
+
 module.exports = {
   defaultQuality: 80,
   ext: 'png',
-  isDelete: true,
+  isDelete: false,
+  db: dbConfig[NODE_ENV],
 }
