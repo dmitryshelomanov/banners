ab091c4c9bb5b4fda86453d9183a834edc4aa1b6
Shelomanov <yzdrf98@mail.ru>
2018-01-17 14:05:29 +0300

    add hook from firmware

---
+++ server/src/utils/firmware/hooks/substitution.js
@@ -0,0 +1,23 @@
+const cheerio = require('cheerio')
+
+
+module.exports = ({
+  content,
+}, html) => new Promise(async (res, rej) => {
+  const $ = cheerio.load(html)
+
+  try {
+    $('script').each(function each() {
+      const $script = $(this)
+
+      if ($script.attr('src') && $script.attr('src').match(/libs/)) {
+        $script.remove()
+      }
+    })
+    $($('script').first().before(content))
+    res($.html())
+  }
+  catch (error) {
+    rej(error)
+  }
+})
