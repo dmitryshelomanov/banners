a06473615dbff60582304b79d0ec846a74145e79
Shelomanov <userbq201@mail.ru>
2018-01-19 16:28:23 +0300

    main server refactore

--- server/index.js
+++ server/index.js
@@ -20,17 +20,24 @@ const server = Server(app.callback())
 const io = socket(server)
 const folder = {}
 
+function deleteCacheHelper(s) {
+  if (!isDelete) return
+  if (typeof folder[s.id] !== 'undefined') {
+    cacheDelete(folder[s.id].nameFolder)
+    delete folder[s.id]
+  }
+}
+
 io.on('connect', (s) => {
   s.on('disconnect', () => {
-    if (!isDelete) return
-    if (typeof folder[s.id] !== 'undefined') {
-      cacheDelete(folder[s.id].nameFolder)
-      delete folder[s.id]
-    }
+    deleteCacheHelper(s)
   })
   s.on('banner:set-archive-name', (data) => {
     if (!isDelete) return
     folder[s.id] = data
+  })
+  s.on('banner:delete-cache', (data) => {
+    deleteCacheHelper(s)
   })
 })
 
