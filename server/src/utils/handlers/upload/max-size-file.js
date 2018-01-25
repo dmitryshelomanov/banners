c88c0440a406eb0d0cd838a7f83dc2a3a85b3df1
Shelomanov <userbq201@mail.ru>
2018-01-25 16:27:50 +0300

    fix eslint rule

--- server/src/utils/handlers/upload/max-size-file.js
+++ server/src/utils/handlers/upload/max-size-file.js
@@ -1,7 +1,7 @@
 const debug = require('debug')('banner:helpers:upload:maxSizeFile')
 
 
-module.exports = size => async (ctx, next) => {
+module.exports = (size) => async (ctx, next) => {
   const { files } = ctx.request
 
   debug(`check file sizes ${files.archive.size / 1024}`)
